7e2b711085612ea8c921867df433d21de69c087d
Work PC <comoontmb@gmail.com>
2019-01-16 10:49:23 +0200

    Reservation homework in class

---
+++ src/test/java/FinalHomework/pages/RegistrationPage.java
@@ -0,0 +1,10 @@
+package FinalHomework.pages;
+
+public class RegistrationPage {
+
+    private BaseFunctions baseFunctions;
+
+    public RegistrationPage(BaseFunctions baseFunctions) {
+        this.baseFunctions = baseFunctions;
+    }
+}
